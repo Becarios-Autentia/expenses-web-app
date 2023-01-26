45cabe8fc5eb45af31b095f3780acd2ff1ed5622
ignaciogomezoliva <ignacio.gomez.oliva@alumnos.upm.es>
2023-01-26 17:29:18 +0100

    Major styling update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,33 +7,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
 import { MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
-import { FriendDialog } from './home/friend-dialog/friend-dialog.component';
-import { ExpenseDialog } from './home/expense-dialog/expense-dialog.component';
-import { BalanceDialog } from './home/balance-dialog/balance-dialog.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FriendDialog } from './friend-dialog/friend-dialog.component';
+import { ExpenseDialog } from './expense-dialog/expense-dialog.component';
+import { BalanceDialog } from './balance-dialog/balance-dialog.component';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatTableModule} from '@angular/material/table';
+import { MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatIconModule} from '@angular/material/icon';
 
 import { HttpService } from './shared/http-service.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TableComponent } from './home/table/table.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     FriendDialog,
     ExpenseDialog,
     TableComponent,
     BalanceDialog
   ],
   imports: [
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -25,8 +25,6 @@ export class TableComponent implements OnInit{
     )
   }
 
-
-
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
